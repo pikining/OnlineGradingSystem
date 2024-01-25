c5853149963536e248b678c5a34b688f18817087
pikining <107294073+pikining@users.noreply.github.com>
2024-01-25 11:29:03 +0800

    Add files via upload

---
+++ superAdmin/viewStudent.php
@@ -0,0 +1,187 @@
+
+<?php
+
+    include('../includes/dbconnection.php');
+    include('../includes/session.php');
+
+  ?>
+
+<!doctype html>
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <?php include '../includes/title.php';?>
+    <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link  rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    
+    <link rel="stylesheet" href="../assets/css/lib/datatable/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+    <!-- Left Panel -->
+    <?php $page="student"; include 'includes/leftMenu.php';?>
+
+   <!-- /#left-panel -->
+
+<div class="main-content">
+
+
+        <!-- Header-->
+            <?php include 'includes/header.php';?>
+        <!-- /header -->
+        <main>
+            <div class="card-head">
+                <div class="page-title">
+                    <h2>Student</h2>
+                </div>
+                <div class="page-header">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item">Dashboard</li>
+                                <li class="breadcrumb-item">Student</li>
+                                <li class="breadcrumb-item active" aria-current="page">View Student</li>
+                          </ol>
+                        </nav>
+                </div>
+            </div>
+
+        <div class="content">
+                    <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong class="card-title"><h2 align="center">All Student</h2></strong>
+                        </div>
+                        <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="bootstrap-data-table" class="table table-hover table-striped table-bordered">
+                                <thead>
+                                <tr>
+                                        <th>#</th>
+                                        <th>FullName</th>
+                                        <th>LRN</th>
+                                        <th>Date Created</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                  
+                                    <?php
+                                        $ret=mysqli_query($con,"SELECT * From tblstudent");
+                                        $cnt=1;
+                                        while ($row=mysqli_fetch_array($ret)) {
+                                     ?>
+                                    <tr>
+                                        <td><?php echo $cnt;?></td>
+                                        <td><?php echo $row['firstName'].' '.$row['lastName'].' '.$row['middleName'];?></td>
+                                        <td><?php echo $row['LRN'];?></td>
+                                        <td><?php echo $row['dateCreated'];?></td>
+                                        <td><a href="editStudent.php?editStudentId=<?php echo $row['LRN'];?>" title="Edit Details"><i class="las la-edit"></i></a>
+                                        <a href="viewStudentinfo.php?editid=<?php echo $row['LRN'];?>" title="Edit Faculty Details"><i class="las la-info-circle"></i></a>
+                                        <a class="delete" data-href="deleteStudent.php?delid=<?php echo $row['LRN'];?>" title="Delete Student Details"><i class="las la-trash-alt"></i></a></td>
+                                    </tr>
+                                    <?php 
+                                        $cnt=$cnt+1;
+                                    }?>
+                                                                                        
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+<!-- end of datatable -->
+</div><!-- .content -->
+
+<div class="clearfix"></div>
+
+</main>
+<!-- FOOTER-->
+    <?php include 'includes/footer.php';?>
+<!-- END OF FOOTER-->
+
+</div><!-- /#right-panel -->
+
+<!-- Right Panel -->
+
+<!-- SCRIPT-->
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
+<script src="../assets/js/main.js"></script>
+
+<script src="../assets/js/lib/data-table/datatables.min.js"></script>
+<script src="../assets/js/lib/data-table/dataTables.bootstrap.min.js"></script>
+<script src="../assets/js/lib/data-table/dataTables.buttons.min.js"></script>
+<script src="../assets/js/lib/data-table/buttons.bootstrap.min.js"></script>
+<script src="../assets/js/lib/data-table/jszip.min.js"></script>
+<script src="../assets/js/lib/data-table/vfs_fonts.js"></script>
+<script src="../assets/js/lib/data-table/buttons.html5.min.js"></script>
+<script src="../assets/js/lib/data-table/buttons.print.min.js"></script>
+<script src="../assets/js/lib/data-table/buttons.colVis.min.js"></script>
+<script src="../assets/js/init/datatables-init.js"></script>
+
+    <script>
+       // Menu Trigger
+        $('#menuToggle').on('click', function(event) {
+        var windowWidth = $(window).width();   		 
+        if (windowWidth<1010) { 
+            $('body').removeClass('open'); 
+            if (windowWidth<760){ 
+            $('#left-panel').slideToggle(); 
+            } else {
+            $('#left-panel').toggleClass('open-menu');  
+            } 
+        } else {
+            $('body').toggleClass('open');
+            $('#left-panel').removeClass('open-menu');  
+        } 
+            
+        }); 
+
+
+    </script>
+
+
+<script type="text/javascript">
+    $(document).ready(function() {
+      $('#bootstrap-data-table-export').DataTable();
+  } );
+
+  
+</script>
+
+
+
+<script src=../assets/plugins/bootstrap-sweetalert/sweetalert.min.js></script>
+<script src="../_common/delete_swal.js"></script>
+
+
+<script>
+    let arrow = document.querySelectorAll(".arrow");
+    console.log(arrow);
+    for (var i = 0; i < arrow.length; i++){
+        arrow[i].addEventListener("click", (e)=>{
+        let arrowParent = e.target.parentElement.parentElement
+            arrowParent.classList.toggle("showMenu");
+        });
+    }
+
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".la-bars");
+    console.log(sidebarBtn);
+    sidebarBtn.addEventListener("click",()=>{
+        sidebar.classList.toggle("close");
+    });
+
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<!-- END OF SCRIPT-->
+
+</body>
+</html>
